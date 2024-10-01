4547cad6c2383e286034e49551c4f9f75751de0e
Bensavage254 <felixomondi186@gmail.com>
2024-10-01 21:59:04 +0300

    updated email

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -15,8 +15,8 @@ export function Contact(){
       </header>
       <div className="contacts">
         <div>
-        <a href="mailto:felixomondi186@.com"><img src={emailIcon} alt="Email" /></a> 
-          <a href="mailto:felixomondi186@.com">felixomondi186@.com</a>
+        <a href="mailto:felixomondi186@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:felixomondi186@gmail.com">felixomondi186@gmail.com</a>
         </div>
         <div>
         <a href="tel:+254745919342"><img src={phoneIcon} alt="Phone No" /></a>
